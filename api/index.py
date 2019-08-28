fba63ff087048508c17e4ec31e56e5e76ed9ace2
JB <jb.tellez@gmail.com>
2019-08-27 22:56:15 -0700

    fixes seat jumping

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ class handler(BaseHTTPRequestHandler):
 
                 if cell is 'seat' and new_y is player_pos['y']:
                     valid_move = True
-                elif not cell in ('facility','wall'):
+                elif seat is 'aisle':
                     valid_move = True
                     
 
